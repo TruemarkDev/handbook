eeee7e5a2965ebbce45af6956336cb900cbfbb59
Prakash Poudel <prakash@truemark.com.np>
2020-12-11 03:52:13 +0545

    Blogging guidelines migration Migrate some file from wiki

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
         name: "Truemark Technology Handbook",
         description: "Handbook is a living document guiding our team members to work together and fulfill our mission.",
         siteUrl: "https://handbook.truemark.dev",
-        sections: ['Playbook', 'Methodology', 'Process', 'Guidelines', 'Folder Structure'],
+        sections: ['Playbook', 'Methodology', 'Process', 'Guidelines', 'Blogging Guidelines'],
       },
     },
   ],
