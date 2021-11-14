5f21e10f6eed7c5c29c37d66a2cddb6c83604cf0
Prakash Poudel <prakash@truemark.com.np>
2021-11-14 15:50:57 +0545

    fix config for gatsby-plugin-sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,25 @@ module.exports = {
       resolve: "smooth-doc",
       options: {
         name: "Truemark Technology Handbook",
-        description: "Handbook is a living document guiding our team members to work together and fulfill our mission.",
+        description:
+          "Handbook is a living document guiding our team members to work together and fulfill our mission.",
         siteUrl: "https://handbook.truemark.dev",
-        sections: ['Handbook', 'Company', 'Benefits', 'Guidelines'],
+        sections: ["Handbook", "Company", "Benefits", "Guidelines"],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: true,
+        // deprecated options and their defaults:
+        base64Width: 20,
+        forceBase64Format: `png`, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
       },
     },
   ],
